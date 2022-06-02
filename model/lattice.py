6b01421c2187104ebc7fc0264a351755e95eba81
Alex Sytchev <sytchevalexander@gmail.com>
2022-06-02 16:13:29 +0200

    Small refactoring

--- model/lattice.py
+++ model/lattice.py
@@ -1,6 +1,5 @@
 #!/usr/local/bin/python3
 
-import matplotlib.pyplot as plt
 from copy import deepcopy
 from model.cell import Cell, create_cells, get_gates, entangle_multiple
 from model.gate import Identity
@@ -81,33 +80,3 @@ class Lattice:
             return 0
 
         return (max([len(cell.entanglements) for cell in self.cells]) + 1) // 2
-
-
-def plot_evolution(automaton, init_config, max_t, debug=False):
-    def list_to_str(l):
-        return " ".join([str(e) for e in l])
-
-    lattice = Lattice(init_config, automaton)
-    res = lattice.iterate(max_t, debug)
-
-    for cells, _ in res:
-        print(list_to_str(cells))
-
-
-def plot_entanglement(alms, init_config, max_t, debug=False):
-    plt.xlabel("Time t")
-    plt.ylabel("Entanglement E(t)")
-
-    for alm in alms:
-        a, l, m = alm
-
-        if debug:
-            print(l)
-
-        lattice = Lattice(init_config, a, True)
-        _, entanglement = zip(*lattice.iterate(max_t, debug))
-
-        plt.plot(list(entanglement), "-" + m, label=l)
-
-    plt.legend()
-    plt.show()
---
+++ plotter.py
@@ -0,0 +1,37 @@
+#!/usr/local/bin/python3
+
+import matplotlib.pyplot as plt
+from model.automaton import Automaton
+from model.lattice import Lattice
+
+
+def plot_evolution(automaton, init_config, max_t, debug=False):
+    def list_to_str(l):
+        return " ".join([str(e) for e in l])
+    
+    a = Automaton(automaton)
+    lattice = Lattice(init_config, a)
+    res = lattice.iterate(max_t, debug)
+
+    for cells, _ in res:
+        print(list_to_str(cells))
+
+
+def plot_entanglement(alms, init_config, max_t, debug=False):
+    plt.xlabel("Time t")
+    plt.ylabel("Entanglement E(t)")
+
+    for alm in alms:
+        a, l, m = alm
+
+        if debug:
+            print(l)
+
+        automaton = Automaton(a)
+        lattice = Lattice(init_config, automaton, True)
+        _, entanglement = zip(*lattice.iterate(max_t, debug))
+
+        plt.plot(list(entanglement), "-" + m, label=l)
+
+    plt.legend()
+    plt.show()
